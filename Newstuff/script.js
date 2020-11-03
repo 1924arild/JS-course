9a918e0553a9e72840b73c7f33a001a63fbea007
Arild Iveren <arild.bus@gmail.com>
2020-11-03 13:49:54 +0100

    second

---
+++ Newstuff/script.js
@@ -0,0 +1,54 @@
+"use strict";
+//alert (`Hello`);
+//const result = confirm("Are you here");
+//console.log(result);
+//const answer = +prompt("Are you 18 years old?","Yes");
+//console.log(answer + 5);
+//const answer = [];
+//answer  [0] = prompt(`What is your name?`,"");
+//answer  [1] = prompt(`What is your family name?`,"");
+//answer  [2] = prompt(`What is your age?`,"");
+//document.write(answer);
+//console.log(typeof(null)); fail!!!
+//console.log(typeof(answer));
+//const category ="toys";
+//console.log("https:someurl.com/" + category);
+//console.log(`https:someurl.com/${category}/5`);
+//const user = "Ivan";
+//alert(`Hello,${user}`);
+//console.log("arr"+"-object");
+//console.log(4 + +"-object");
+//console.log(4 + +"5");
+
+let incr = 10,
+    decr =10;
+//incr++;//postficnaja
+//decr--;//postficnaja
+//++incr;
+//--decr; preficsnaja
+//console.log(incr);
+//console.log(decr);
+//++incr;
+//--decr;
+//console.log(incr++);//first taking back old numbers
+//console.log(decr--);//first taking back old numbers
+console.log(++incr);
+console.log(--decr);
+console.log(5%2);//octatok
+//console.log(2*4 == 8);
+console.log(2*4 == `8`);//true, but
+console.log(2*4 === `8`);// ctrogoe ravenstovo srvniavaet tipu dannux i false cejchas
+const //isChecked = true,
+      //isClose = true;
+      //isClose = false;
+      isChecked = false,
+      isClose = false;
+console.log(isChecked && isClose);
+console.log(isChecked || !isClose);//! operator otrizanija
+//console.log(2+2*2 === 8);
+//console.log(2+2*2 != 8);
+console.log(2+2*2 != "6");//false
+console.log(2+2*2 !== "6");//true
+
+
+
